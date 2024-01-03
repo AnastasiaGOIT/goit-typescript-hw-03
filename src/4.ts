03030d811d6922790820bd96a5ddd74bafaeafa7
AnastasiaGOIT <hudzevych.music@gmail.com>
2024-01-03 19:05:51 +0200

    done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,43 @@
+class Key {
+  private signature = Math.random();
+
+  getSignature() {
+    return this.signature;
+  }
+}
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean;
+  key: Key;
+  tetants: Person[] = [];
+
+  comeIn(person: Person) {
+    if (this.door === true) {
+      this.tetants.push(person);
+    }
+  }
+  openDoor(key: Key): void {}
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+  }
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +46,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
